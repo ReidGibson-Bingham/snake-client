17bc2d1c20b2d66849ed6fe880d68d1e515b3209
P4ulH1ll <jsr3b0rn@gmail.com>
2022-02-17 05:41:39 +0000

    snake game

--- play.js
+++ play.js
@@ -14,3 +14,22 @@ for (let i = 0, milS = 0; i < 10; i++, milS += 100) {
     connect(moves[Math.floor(Math.random() * 4)]);
   }, milS)
 }
+
+const handleUserInput = function (input) {
+  if (input.keyCode === 67) {
+    process.exit();
+  }
+};
+// ^ this function checks for ctrl+c with input.keyCode === 67
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+setupInput();
