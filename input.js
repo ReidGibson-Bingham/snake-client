e664e5c8a26b0e5fe7f26e3ad256a66954d43d1f
P4ulH1ll <jsr3b0rn@gmail.com>
2022-02-17 06:38:37 +0000

    i'll need to work on this assignment a lot more

--- input.js
+++ input.js
@@ -1,3 +1,6 @@
+//const { connect } = require("./client");
+let connection;
+
 const handleUserInput = function (input) {
   if (input.keyCode === 67) {
     process.exit();
@@ -5,7 +8,8 @@ const handleUserInput = function (input) {
 };
 // ^ this function checks for ctrl+c with input.keyCode === 67
 
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -15,6 +19,36 @@ const setupInput = function () {
   return stdin;
 };
 
+
 module.exports = {
   setupInput
 }
+
+const { connect } = require('http2');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let moves = ['Move: up', 'Move: down', 'Move: left', 'Move: right'];
+
+rl.question('Enter directional input ', (dir) => {
+  if (dir === 'w') {
+    connect(moves[0]);
+  }
+  if (dir === 'a') {
+    connect(moves[1]);
+  }
+  if (dir === 's') {
+    connect(moves[2])
+  }
+  if (dir === 'd') {
+    connect(moves[3])
+  }
+  
+  
+  rl.close();
+});
+  
--- play.js
+++ play.js
@@ -6,6 +6,9 @@ const { setupInput } = require("./input");
 console.log("Connecting ...");
 connect();
 
+//v playing around with directions
+
+/*
 let moves = ['Move: up', 'Move: down', 'Move: left', 'Move: right'];
 
 connect(moves[Math.floor(Math.random() * 4)]);
@@ -15,26 +18,14 @@ for (let i = 0, milS = 0; i < 10; i++, milS += 100) {
     connect(moves[Math.floor(Math.random() * 4)]);
   }, milS)
 }
-
+*/
 
 const handleUserInput = function (input) {
   if (input.keyCode === 67) {
     process.exit();
   }
 };
-// ^ this function checks for ctrl+c with input.keyCode === 67
+// ^ this function checks for ctrl+c with input.keyCode === 67, it's a callback for the setupInput function
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
 
-  return stdin;
-};
-
-console.log("Connecting ...");
-connect();
-
-setupInput();
+setupInput(connect('test'));
